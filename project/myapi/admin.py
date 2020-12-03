58322de5e8791f3ad6d510fb54b815491049aef7
Kashish Ahuja <59805144+kashishahuja02@users.noreply.github.com>
2020-12-03 14:51:04 +0530

    Add files via upload

---
+++ project/myapi/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib import admin
+from myapi.models import student,teacher
+admin.site.register(student)
+admin.site.register(teacher)
+
+# Register your models here.
